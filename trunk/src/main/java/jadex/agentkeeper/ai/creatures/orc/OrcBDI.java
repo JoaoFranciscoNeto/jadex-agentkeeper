a315d23bf525f76ac7eb59704060c4d66500eb1d
PhilWilluweit <PhilWilluweit@1b6f9c2a-31fe-ed52-ef1d-3333e6b1e282>
2013-02-03 13:54:11 +0000

    Initial Commit

---
+++ trunk/src/main/java/jadex/agentkeeper/ai/creatures/orc/OrcBDI.java
@@ -0,0 +1,17 @@
+package jadex.agentkeeper.ai.creatures.orc;
+
+
+import jadex.agentkeeper.ai.creatures.AbstractCreatureBDI;
+
+
+/**
+ * The ORC
+ * 
+ * @author Philip Willuweit p.willuweit@gmx.de
+ */
+
+public class OrcBDI extends AbstractCreatureBDI
+{
+
+
+}
