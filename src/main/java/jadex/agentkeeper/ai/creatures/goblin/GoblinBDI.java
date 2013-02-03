9950b0f6d82599672e4d6ea24873e564e0f1cfb6
PhilWilluweit <PhilWilluweit@1b6f9c2a-31fe-ed52-ef1d-3333e6b1e282>
2013-02-03 13:54:11 +0000

    Initial Commit

---
+++ src/main/java/jadex/agentkeeper/ai/creatures/goblin/GoblinBDI.java
@@ -0,0 +1,16 @@
+package jadex.agentkeeper.ai.creatures.goblin;
+
+import jadex.agentkeeper.ai.creatures.AbstractCreatureBDI;
+
+
+/**
+ * The GOBLIN
+ * 
+ * @author Philip Willuweit p.willuweit@gmx.de
+ */
+
+public class GoblinBDI extends AbstractCreatureBDI
+{
+
+
+}
