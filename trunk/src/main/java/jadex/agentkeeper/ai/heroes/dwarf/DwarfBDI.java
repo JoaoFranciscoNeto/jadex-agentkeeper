4c37387ca18d8ef41d15e6a057f7ed64fb3e0f46
PhilWilluweit <PhilWilluweit@1b6f9c2a-31fe-ed52-ef1d-3333e6b1e282>
2013-02-07 16:00:35 +0000

    Some Cleanup

---
+++ trunk/src/main/java/jadex/agentkeeper/ai/heroes/dwarf/DwarfBDI.java
@@ -0,0 +1,16 @@
+package jadex.agentkeeper.ai.heroes.dwarf;
+
+import jadex.agentkeeper.ai.heroes.AbstractHeroBDI;
+
+
+/**
+ * The Dwarf
+ * 
+ * @author Philip Willuweit p.willuweit@gmx.de
+ */
+
+public class DwarfBDI extends AbstractHeroBDI
+{
+
+
+}
---
+++ trunk/src/main/java/jadex/agentkeeper/ai/heroes/knight/KnightBDI.java
@@ -0,0 +1,16 @@
+package jadex.agentkeeper.ai.heroes.knight;
+
+import jadex.agentkeeper.ai.heroes.AbstractHeroBDI;
+
+
+/**
+ * The Knight
+ * 
+ * @author Philip Willuweit p.willuweit@gmx.de
+ */
+
+public class KnightBDI extends AbstractHeroBDI
+{
+
+
+}
