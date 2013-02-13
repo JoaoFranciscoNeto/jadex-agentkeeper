3ab6ae4a1e4f73d640c6e2ff0dfc9f6787b703a7
PhilWilluweit <PhilWilluweit@1b6f9c2a-31fe-ed52-ef1d-3333e6b1e282>
2013-02-13 21:59:46 +0000

    TileInfo

--- src/main/java/jadex/agentkeeper/worldmodel/buildingtypes/DungeonHeartInfo.java
+++ src/main/java/jadex/agentkeeper/worldmodel/buildingtypes/DungeonHeartInfo.java
@@ -4,15 +4,11 @@ import jadex.agentkeeper.worldmodel.enums.MapType;
 import jadex.agentkeeper.worldmodel.enums.NeighbourType;
 
 public class DungeonHeartInfo extends ACenterBuildingInfo {
-	
 
-	
 	public DungeonHeartInfo(MapType mapType)
 	{
 		super(mapType);
 		this.hitpoints = isCenter? 1000 : 10;
 		this.neighbourType = NeighbourType.COMPLEX;
 	}
-	
-
 }
---
+++ src/main/java/jadex/agentkeeper/worldmodel/solidtypes/WaterInfo.java
@@ -0,0 +1,14 @@
+package jadex.agentkeeper.worldmodel.solidtypes;
+
+import jadex.agentkeeper.worldmodel.SolidInfo;
+import jadex.agentkeeper.worldmodel.enums.MapType;
+
+public class WaterInfo extends SolidInfo
+{
+
+	public WaterInfo(MapType mapType)
+	{
+		super(mapType);
+	}
+
+}
